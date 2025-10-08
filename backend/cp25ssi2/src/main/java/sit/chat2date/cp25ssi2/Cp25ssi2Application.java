8aa22c55b2a4785576980258c6355dd8d6c11ab6
h88ych <hathaitat88@gmail.com>
2025-10-08 20:59:42 +0700

    setup(backend)

---
+++ backend/cp25ssi2/src/main/java/sit/chat2date/cp25ssi2/Cp25ssi2Application.java
@@ -0,0 +1,13 @@
+package sit.chat2date.cp25ssi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cp25ssi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cp25ssi2Application.class, args);
+	}
+
+}
---
+++ backend/cp25ssi2/src/test/java/sit/chat2date/cp25ssi2/Cp25ssi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sit.chat2date.cp25ssi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cp25ssi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
