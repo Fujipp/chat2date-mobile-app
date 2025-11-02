a410efb2502c3b6f3c9cab4f0426e79cbec25020
h88ych <hathaitat88@gmail.com>
2025-11-02 11:59:29 +0700

    update(mobile) : main add comp button

--- frontend-mobile/chat2date/lib/main.dart
+++ frontend-mobile/chat2date/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:chat2date/theme/app_theme.dart'; // <— เพิ่ม
+import 'package:chat2date/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/index.dart';
@@ -10,17 +10,46 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static final navigatorKey = GlobalKey<NavigatorState>();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Component Test',
       theme: buildLightTheme(),
-      initialRoute: '/test',
+      navigatorKey: navigatorKey,
+      initialRoute: '/profile',
       routes: {
         '/test': (context) => const ComponentTestScreen(),
         '/profile': (context) => const ProfileSetupScreen(),
       },
-      // home: ComponentTestScreen(),
+      //ปุ่มไว้สำหรับดู comp
+      builder: (context, child) {
+        return Stack(
+          children: [
+            child ?? const SizedBox(),
+            Positioned(
+              right: 16,
+              bottom: 16,
+              child: FloatingActionButton(
+                onPressed: () {
+                  final nav = navigatorKey.currentState!;
+                  if (nav.canPop()) {
+                    nav.pop(); // 🔹 ถ้ามีหน้าก่อนหน้า -> กลับ
+                  } else {
+                    nav.pushNamed(
+                      '/test',
+                    ); // 🔹 ถ้ายังอยู่หน้าแรก -> ไปหน้า test
+                  }
+                },
+                backgroundColor: Colors.blueAccent,
+                heroTag: 'globalTestBtn',
+                child: const Icon(Icons.help, color: Colors.white),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
