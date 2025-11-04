00f68ff1bd9e1abcec3b8f4febab516b56a92e8b
hutchz72 <varittorn.siri@mail.kmutt.ac.th>
2025-11-05 03:59:24 +0700

    update(mobile): path

--- frontend-mobile/chat2date/lib/main.dart
+++ frontend-mobile/chat2date/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: buildLightTheme(),
       navigatorKey: navigatorKey,
       initialRoute:
-          '/discovery', //เวลาโค้ดเปลี่ยนเป็น path ตัวเองเอาไว้แสดงหน้าของตัวเองเด้อ
+          '/test', //เวลาโค้ดเปลี่ยนเป็น path ตัวเองเอาไว้แสดงหน้าของตัวเองเด้อ
       routes: {
         //Test
         '/test': (context) => const ComponentTestScreen(),
