a973379d7b3420f236d8cf9d7726c49e828f941d
h88ych <hathaitat88@gmail.com>
2025-11-08 13:24:33 +0700

    feat(backend) : draft secureconfig

---
+++ backend/cp25ssi2/src/main/java/sit/chat2date/cp25ssi2/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package sit.chat2date.cp25ssi2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/users").permitAll() //รอ token ค่อยดัก
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
