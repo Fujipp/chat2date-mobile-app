93ce377b6c41348b1a584a217bf7286d4cd8d140
hutchz72 <varittorn.siri@mail.kmutt.ac.th>
2025-11-16 17:08:17 +0700

    update(frontend): remove isVerify age and faceVerify

--- frontend-mobile/chat2date/lib/models/user.dart
+++ frontend-mobile/chat2date/lib/models/user.dart
@@ -10,15 +10,12 @@ class User {
   final String userId;
   final String? email;
   final String? phoneNumber;
-  final bool? isVerify;
   final Provider? provider;
   final String? firstname;
   final String? lastname;
   final String? cardId;
   final DateTime? birthday;
-  final int? age;
   final Sex? sex;
-  final bool? faceVerify;
   final int? behaviorScore;
   final bool? isBlacklist;
   final AccountStatus? accountStatus;
@@ -27,16 +24,13 @@ class User {
 
   const User({
     this.accountStatus,
-    this.age,
     required this.userId,
     this.behaviorScore,
     this.birthday,
     this.cardId,
     this.email,
-    this.faceVerify,
     this.firstname,
     this.isBlacklist,
-    this.isVerify,
     this.lastname,
     this.phoneNumber,
     this.provider,
@@ -61,15 +55,12 @@ class User {
       userId: json['userId'],
       email: json['email'],
       phoneNumber: json['phoneNumber'],
-      isVerify: json['isVerify'],
       provider: enumFromString(Provider.values, json['provider']),
       firstname: json['firstname'],
       lastname: json['lastname'],
       cardId: json['cardId'],
       birthday: parseDate(json['birthday']),
-      age: json['age'],
       sex: enumFromString(Sex.values, json['sex']),
-      faceVerify: json['faceVerify'],
       behaviorScore: json['behaviorScore'],
       isBlacklist: json['isBlacklist'],
       accountStatus: enumFromString(
@@ -86,15 +77,12 @@ class User {
     'userId': userId,
     'email': email,
     'phoneNumber': phoneNumber,
-    'isVerify': isVerify,
     'provider': provider?.toString().split('.').last,
     'firstname': firstname,
     'lastname': lastname,
     'cardId': cardId,
     'birthday': birthday?.toIso8601String(),
-    'age': age,
     'sex': sex?.toString().split('.').last,
-    'faceVerify': faceVerify,
     'behaviorScore': behaviorScore,
     'isBlacklist': isBlacklist,
     'accountStatus': accountStatus?.toString().split('.').last,
