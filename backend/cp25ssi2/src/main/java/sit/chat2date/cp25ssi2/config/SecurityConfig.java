7aeee7b3541134efe6c22e0c5d65cbe48d4f9b23
h88ych <hathaitat88@gmail.com>
2026-02-05 17:25:04 +0700

    fix(backend) : allow test

--- backend/cp25ssi2/src/main/java/sit/chat2date/cp25ssi2/config/SecurityConfig.java
+++ backend/cp25ssi2/src/main/java/sit/chat2date/cp25ssi2/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                                 .requestMatchers("/auth/**").permitAll()
                                 .requestMatchers("/ws/**").permitAll()
                                 .requestMatchers("/users/*/restore").permitAll()
-                                .requestMatchers("//test/**").permitAll()
+                                .requestMatchers("/test/**").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/users/phone").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/users").hasRole(Role.ADMIN.name())
                                 .anyRequest().hasAnyRole(Role.USER.name(), Role.ADMIN.name())
