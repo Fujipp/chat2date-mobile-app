03a74e67a92a35b41bef23604ccf5d0d762cf83a
h88ych <hathaitat88@gmail.com>
2026-03-09 18:07:14 +0700

    feat(backend) : date controller

---
+++ backend/cp25ssi2/src/main/java/sit/chat2date/cp25ssi2/controllers/DateController.java
@@ -0,0 +1,10 @@
+package sit.chat2date.cp25ssi2.controllers;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/dates")
+@RequiredArgsConstructor
+public class DateController {
+
+}
