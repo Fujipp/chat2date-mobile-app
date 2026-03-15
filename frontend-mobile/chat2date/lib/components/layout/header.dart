7710344943eff6408830467969559e38c346b2d5
hutchz72 <varittorn.siri@mail.kmutt.ac.th>
2026-03-16 00:30:37 +0700

    update(mobile): spinwheel cooldown

--- frontend-mobile/chat2date/lib/components/layout/header.dart
+++ frontend-mobile/chat2date/lib/components/layout/header.dart
@@ -343,8 +343,8 @@ class _CooldownSpinwheelIcon extends StatelessWidget {
           ),
           // Cooldown number badge ที่ด้านบน
           Positioned(
-            top: -2,
-            left: 9,
+            top: 5,
+            left: 0,
             child: Container(
               width: 7,
               height: 10,
