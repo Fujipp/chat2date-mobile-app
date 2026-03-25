dd58385bd65207b7f2495e9b98e14fd0096ab57b
hutchz72 <varittorn.siri@mail.kmutt.ac.th>
2026-03-25 16:00:39 +0700

    fix+update(mobile): using th sms

--- frontend-mobile/chat2date/lib/screens/auth/phone_page.dart
+++ frontend-mobile/chat2date/lib/screens/auth/phone_page.dart
@@ -82,14 +82,14 @@ class _PhonePageState extends State<PhonePage> {
       }
 
       // STEP 2 — ส่ง OTP
-      final token = await BackendOtpService.sendOtp(phone, context);
+      final response = await BackendOtpService.sendOtp(phone, context);
 
-      if (token != "") {
+      if (response != "") {
         if (!mounted) return;
         Navigator.pushNamed(
           context,
           '/otp',
-          arguments: {'token': token, 'phone': phone, 'onLogin': onLogin},
+          arguments: {'phone': phone, 'onLogin': onLogin},
         );
       }
     } catch (e) {
