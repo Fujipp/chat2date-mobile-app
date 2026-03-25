9c8185568ae3307537bd50f71918a36251597b4d
h88ych <hathaitat88@gmail.com>
2026-03-25 23:42:31 +0700

    feat(backend) : contact SOS Topic

--- backend/cp25ssi2/src/main/java/sit/chat2date/cp25ssi2/repositories/SosIncidentRepository.java
+++ backend/cp25ssi2/src/main/java/sit/chat2date/cp25ssi2/repositories/SosIncidentRepository.java
@@ -5,14 +5,8 @@ import sit.chat2date.cp25ssi2.entities.SosIncident;
 import sit.chat2date.cp25ssi2.enums.SosStatus;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SosIncidentRepository extends JpaRepository<SosIncident, Integer> {
-
-    List<SosIncident> findByAppointment_AppointmentId(Integer appointmentId);
-
-    List<SosIncident> findByReporterId(String reporterId);
-
-    List<SosIncident> findByTargetUserId(String targetUserId);
-
-    List<SosIncident> findByStatus(SosStatus status);
+    Optional<SosIncident> findFirstByReporterIdOrderByIncidentIdDesc(String reporterId);
 }
