2ab1c8d8580fc5b0c2ec2aed71726fb333d52025
hutchz72 <varittorn.siri@mail.kmutt.ac.th>
2026-03-25 15:59:47 +0700

    update(backend): TH sms

--- backend/cp25ssi2/src/main/java/sit/chat2date/cp25ssi2/dto/OtpValidateRequest.java
+++ backend/cp25ssi2/src/main/java/sit/chat2date/cp25ssi2/dto/OtpValidateRequest.java
@@ -6,8 +6,6 @@ import sit.chat2date.cp25ssi2.enums.Role;
 @Data
 public class OtpValidateRequest {
     private String phoneNumber;
-    private String token;    // จาก otp-send
     private String otpCode; // โค้ดที่ผู้ใช้กรอก
     private boolean onLogin;
-    private String refCode;  // optional
 }
